35eb2e6c2eb10d382a486866b889b26bc57937ed
joao.nisa <joao.nisa@primer.io>
2024-08-02 15:21:49 +0100

    updated TODO list + code examples for the restructure

--- README.md
+++ README.md
@@ -3,3 +3,37 @@
 **TODO** 
 - Use [ebitengine](https://ebitengine.org/) to play the game;
 - Create a Makefile to automatically install the dependencies;
+
+**Changes on the current version of the code**
+- All the functions that envolve manipulating the board should be modified to leverage pointers;
+- The inputs from the keyboard should be detected by using the `ebitengine` package;
+- Check if there's functions that can be excluded cause they're not being used;
+- Check if there's function that can be changed from the following:
+
+```
+func GetPositionValue(b Board, x_coord int, y_coord int) int {
+    return Board.Cells[x_coord][y_coord]
+}
+```
+
+to
+
+```
+func (b Board) GetPositionValue (x_coord int, y_coord int) int{
+    return Board.Cells[x_coord][y_coord]
+}
+```
+
+they both do the same thing, but we should get use to the Golang notation;
+- [DONE] Reconsider the structure of the project - most of the repos I've checked online would have the 
+`auxiliars.go`, `board.go`, `inputs.go`, etc scripts inside one directory only;
+`Note`: according to this article the structure that we have atm is the one that follows the best practises.
+
+**Documentation with Go best-practises**
+- [Golang official documentation](https://go.dev/doc/effective_go);
+- 
+
+
+**Learning resources**
+
+1. [Pointers in Golang: when, why, and how to use them?](https://www.youtube.com/watch?v=3WsEDZRif6U)
--- cmp/snake/main.go
+++ cmp/snake/main.go
@@ -1,86 +1,139 @@
-// Main Heart of the snake game
+// // Main Heart of the snake game
+
+// package main
+
+// import (
+// 	"fmt"
+// 	"time"
+
+// 	"github.com/jnisa/snake-go/pkg/auxiliars"
+// 	"github.com/jnisa/snake-go/pkg/inputs"
+// 	"github.com/jnisa/snake-go/pkg/moves"
+// 	"github.com/jnisa/snake-go/pkg/objects"
+// 	"github.com/jnisa/snake-go/pkg/states"
+// )
+
+// func main() {
+
+// 	var board objects.Board
+
+// 	snake := objects.Snake{
+// 		Body:      [][]int{{12, 16}, {11, 16}, {10, 16}},
+// 		Direction: objects.Right,
+// 		Score:     0,
+// 	}
+
+// 	ticker := time.NewTicker(500 * time.Millisecond) // Snake moves every 500ms
+// 	defer ticker.Stop()
+
+// 	done := make(chan bool)
+// 	exit := make(chan bool)
+
+// 	go func() {
+
+// 		defer close(done)
+
+// 		// get a new food position and add it to the board
+// 		food_x, food_y := auxiliars.GetRandomPosition(snake, board)
+// 		board.Cells[food_x][food_y] = 1
+
+// 		for {
+
+// 			select {
+// 			case <-exit:
+// 				fmt.Println("Exiting gamehelp...")
+// 				return
+// 			case <-ticker.C:
+
+// 				// update snake's body in case there's any ingestion
+// 				snake, board = states.SnakeIngestionUpdate(snake, board)
+
+// 				// check if the game is over
+// 				if states.IsGameOver(snake, board) {
+// 					fmt.Println("Game Over")
+// 					break
+// 				}
+
+// 				// check if the board has any food
+// 				if !auxiliars.BoardContainsValue(1, board) {
+// 					food_x, food_y = auxiliars.GetRandomPosition(snake, board)
+// 					board.Cells[food_x][food_y] = 1
+// 				}
+
+// 				move := inputs.GetSnakeMove(inputs.InputReader(nil))
+
+// 				// if a key was pressed by the user, then move the snake in the given direction
+// 				// if no key was pressed by the user, then move the snake in the last direction
+// 				switch move {
+// 				case "up":
+// 					snake = moves.MoveUp(snake)
+// 				case "down":
+// 					snake = moves.MoveDown(snake)
+// 				case "left":
+// 					snake = moves.MoveLeft(snake)
+// 				case "right":
+// 					snake = moves.MoveRight(snake)
+// 				case "esc":
+// 					exit <- true
+// 				case "":
+// 					snake = moves.UpdateSnake(snake)
+// 				}
+// 			}
+// 		}
+// 	}()
+
+// 	<-done
+// }
 
 package main
 
 import (
 	"fmt"
-	"time"
+	"log"
 
-	"github.com/jnisa/snake-go/pkg/auxiliars"
-	"github.com/jnisa/snake-go/pkg/inputs"
-	"github.com/jnisa/snake-go/pkg/moves"
-	"github.com/jnisa/snake-go/pkg/objects"
-	"github.com/jnisa/snake-go/pkg/states"
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
-func main() {
+type Game struct{}
 
-	var board objects.Board
-
-	snake := objects.Snake{
-		Body:      [][]int{{12, 16}, {11, 16}, {10, 16}},
-		Direction: objects.Right,
-		Score:     0,
+func (g *Game) Update() error {
+	// Check if the arrow keys are pressed
+	if ebiten.IsKeyPressed(ebiten.KeyArrowLeft) {
+		fmt.Println("Left arrow key pressed")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowRight) {
+		fmt.Println("Right arrow key pressed")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) {
+		fmt.Println("Up arrow key pressed")
+	}
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) {
+		fmt.Println("Down arrow key pressed")
 	}
 
-	ticker := time.NewTicker(500 * time.Millisecond) // Snake moves every 500ms
-	defer ticker.Stop()
+	// Check if the space key was just pressed
+	if inpututil.IsKeyJustPressed(ebiten.KeySpace) {
+		fmt.Println("Space key just pressed")
+	}
 
-	done := make(chan bool)
-	exit := make(chan bool)
+	return nil
+}
 
-	go func() {
+func (g *Game) Draw(screen *ebiten.Image) {
+	// You can draw your game objects here
+}
 
-		defer close(done)
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 640, 480 // return the logical screen size
+}
 
-		// get a new food position and add it to the board
-		food_x, food_y := auxiliars.GetRandomPosition(snake, board)
-		board.Cells[food_x][food_y] = 1
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Key Press Detection Example")
 
-		for {
-
-			select {
-			case <-exit:
-				fmt.Println("Exiting gamehelp...")
-				return
-			case <-ticker.C:
-
-				// update snake's body in case there's any ingestion
-				snake, board = states.SnakeIngestionUpdate(snake, board)
-
-				// check if the game is over
-				if states.IsGameOver(snake, board) {
-					fmt.Println("Game Over")
-					break
-				}
-
-				// check if the board has any food
-				if !auxiliars.BoardContainsValue(1, board) {
-					food_x, food_y = auxiliars.GetRandomPosition(snake, board)
-					board.Cells[food_x][food_y] = 1
-				}
-
-				move := inputs.GetSnakeMove(inputs.InputReader(nil))
-
-				// if a key was pressed by the user, then move the snake in the given direction
-				// if no key was pressed by the user, then move the snake in the last direction
-				switch move {
-				case "up":
-					snake = moves.MoveUp(snake)
-				case "down":
-					snake = moves.MoveDown(snake)
-				case "left":
-					snake = moves.MoveLeft(snake)
-				case "right":
-					snake = moves.MoveRight(snake)
-				case "esc":
-					exit <- true
-				case "":
-					snake = moves.UpdateSnake(snake)
-				}
-			}
-		}
-	}()
-
-	<-done
+	game := &Game{}
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
 }
